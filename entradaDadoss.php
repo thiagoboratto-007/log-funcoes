70abc72d698d96cfc1f086f308314625880bb0cf
thiagoboratto-007 <thiago.docema@edu.senai.br>
2026-04-28 09:27:36 -0300

    atividade função e namespace

---
+++ entradaDadoss.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "bibliotecaFuncoess";
+
+use function geometria\areaQuadrado;
+use function geometria\areaRetangulo;
+use function geometria\areaCirculo;
+use function geometria\areaTriangulo;
+use function geometria\areaTrapezio;
