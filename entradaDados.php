3425540ff4f2f18d73b66d53527367888990f274
thiagoboratto-007 <thiago.docema@edu.senai.br>
2026-04-28 09:50:50 -0300

    atividade função

---
+++ entradaDados.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once "bibliotecaFuncoes.php";
+
+use function conversao\dolarParaReal;
+use function conversao\euroParaReal;
+use function conversao\pesoParaReal;
+use function conversao\libraParaReal;
+use function conversao\ieneParaReal; 
