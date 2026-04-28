3660cfabf0c19a917bb4778b5c841d081afaff36
thiagoboratto-007 <thiago.docema@edu.senai.br>
2026-04-28 09:26:57 -0300

    atividade função

---
+++ bibliotecaFuncoess.php
@@ -0,0 +1,37 @@
+<?php
+
+//Namespace conversao:
+//Função dolarParaReal($valor, $cotacao)
+//Função euroParaReal($valor, $cotacao)
+//Função pesoParaReal($valor, $cotacao)
+//Função libraParaReal($valor, $cotacao)
+//Função ieneParaReal($valor, $cotacao)
+
+
+
+namespace conversao;
+function dolarParaReal ($valor,$cotacao){    
+     return $valor * $cotacao;
+
+
+ }
+
+ namespace conversao;
+ function euroParaReal($valor, $cotacao){
+return $valor * $cotacao;
+
+ }
+ namespace conversao;
+ function pesoParaReal($valor, $cotacao){
+return $valor * $cotacao;
+
+ }
+ namespace conversao;
+ function libraParaReal($valor, $cotacao){
+return $valor * $cotacao;
+
+ }
+  namespace conversao;
+ function ieneParaReal($valor, $cotacao){
+return $valor * $cotacao;
+ }
