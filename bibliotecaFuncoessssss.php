07cc55c3829fcb9407e43f0923c60516cf2fcb29
thiagoboratto-007 <thiago.docema@edu.senai.br>
2026-04-28 11:02:12 -0300

    atividade funções

---
+++ bibliotecaFuncoessssss.php
@@ -0,0 +1,38 @@
+<?php
+//Utilize o arquivo criado chamado "bibliotecaFuncoes.php", desenvolva as funções abaixo:
+
+//Namespace saude:
+
+//Função calcularImc($peso, $altura)
+//Função valorIdealAgua($peso)
+//Função frequenciaCardiacaMaxima($idade)
+//Função converterLibrasParaQuilo($libras)
+//Função calcularCaloriasBasais($peso, $idade, $sexo)
+
+
+namespace saude;
+function calcularImc($peso, $altura){
+  return $peso * $altura  
+
+
+;}
+
+namespace saude;
+function valorIdealAgua($peso){
+return $peso * $peso
+
+;}
+namespace saude;
+function frequenciaCardiacaMaxima($idade){
+return $idade * $idade 
+
+;}
+namespace saude;
+function converterLibrasParaQuilo($libras){
+return $libras / $libras
+
+;}
+namespace saude;
+function calcularCaloriasBasais($peso, $Idade, $sexo){
+ return $peso * $Idade * $sexo   
+;}
---
+++ entradaDadossss.php
@@ -0,0 +1,7 @@
+<?php
+
+use function saude\calcularImc;
+use function saude\valorIdealAgua;
+use function saude\frequenciaCardiacaMaxima;
+use function saude\converterLibrasParaQuilo;
+use function saude\calcularCaloriasBasais;
